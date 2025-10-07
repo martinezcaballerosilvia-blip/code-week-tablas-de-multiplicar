3dc1607fc060ac13698b50887010315c6261c79e
martinezcaballerosilvia-blip <martinezcaballerosilvia@gmail.com>
2025-10-07 18:54:59 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ input.onButtonPressed(Button.A, function () {
     a = randint(1, 10)
     basic.showNumber(a)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(a + b)
+})
 input.onButtonPressed(Button.B, function () {
     b = randint(1, 10)
     basic.showNumber(b)
